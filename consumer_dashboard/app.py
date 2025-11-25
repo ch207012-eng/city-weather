0ee9156197565554f157ecb36272452bbdfa394d
ch207012-eng <ch207012@gmail.com>
2025-11-25 05:56:02 -0600

    Update app.py

--- consumer_dashboard/app.py
+++ consumer_dashboard/app.py
@@ -73,4 +73,4 @@ def update_all(n):
     return current_children, figures
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8050)
